0500f2a8a01e3b9747f456e50cecfea22a7704ff
igawayoshiki <igawayoshiki@users.noreply.github.com>
2017-06-21 09:51:01 +0900

    Update Hello.java

--- Github-example/src/Hello.java
+++ Github-example/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "Hello,Git!!";
-		System.out.println(str);
+		System.out.println(str + "\n" + str);
 		
 	}
 
